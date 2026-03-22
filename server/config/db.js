b45d98b2bbd88d30cfd73cd880c0776cd5f33b69
Christophe177 <nshutichristophe10@gmail.com>
2026-03-22 07:23:36 -1000

    Add support for custom database port

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,7 @@ dotenv.config();
 // Create the connection pool. The pool-specific settings are the defaults
 export const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'church_cms',
